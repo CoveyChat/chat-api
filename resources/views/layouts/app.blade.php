a98054dc68a841fd98409d0f3333bc22bface1b1
Jacob Rogaishio <me@jacobrogaishio.com>
2020-04-02 19:34:05 -0400

    Homepage design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,11 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="icon" href="{{ URL::asset('/img/favicon.png') }}" type="image/x-icon"/>
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Bevy Chat') }} - For the birds</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @component('components.head')
+        <strong>Whoops!</strong> Something went wrong!
+    @endcomponent
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,106 +1,73 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    @component('components.head')
+        <strong>Whoops!</strong> Something went wrong!
+    @endcomponent
 
-        <title>Bevy Chat - A chat for the birds</title>
+    <style>
+        .title {
+            font-size: 84px;
+        }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+        .links {
+            margin-top:20px;
+        }
+        ol {
+            list-style-type: none;
+        }
+    </style>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 text-right links">
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
 
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="m-b-md">
-                    <img src='{{URL::asset('img/logo_color.png')}}' alt='BevyChat Logo' />
-                    <h1 class='title'>Bev·y</h1>
-                    <p>
-                        <span class="text-muted">/ˈbevē/</span>
-                    </p>
-                    <blockquote>
-                        <p>
-                            <em>noun</em>
-                        </p>
-                        <p>
-                            <ul>
-                                <li>
-                                    a large group of people or things of a particular kind.
-                                    "a bevy of big-name cameos will keep the adults entertained"
-                                </li>
-                                <li>
-                                    a group of birds, especially quail, particularly when closely gathered on the ground.
-                                </li>
-                            </ul>
-                        </p>
-                    </blockquote>
-                </div>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
             </div>
         </div>
-    </body>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <img src='{{URL::asset('img/logo_color.png')}}' alt='BevyChat Logo' class='img-fluid mx-auto' />
+                <h1 class='title'>Bev·y</h1>
+                <h2><span class="text-muted">/ˈbevē/</span></h2>
+                <blockquote>
+                    <p>
+                        <h3><em>noun</em></h3>
+                    </p>
+                    <p>
+                        <ol>
+                            <li>
+                                <span class="text-muted">1.</span> large group or collection
+                            </li>
+                            <li>
+                                <span class="text-muted">2.</span> a group of animals and especially quail
+                            </li>
+                            <li>
+                                <span class="text-muted">3.</span> a pretty nice video chat service
+                            </li>
+                        </ul>
+                    </p>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
