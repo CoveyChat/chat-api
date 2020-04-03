a13266803e9d2c2754f07a99a571996df90b2e47
Jacob Rogaishio <me@jacobrogaishio.com>
2020-04-03 13:31:35 -0400

    Welcome page updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 
     <style>
         .title {
-            font-size: 84px;
+            font-size: 5em;
         }
 
         .links > a {
@@ -46,22 +46,43 @@
         <div class="row">
             <div class="col-md-12 text-center">
                 <img src='{{URL::asset('img/logo_color.png')}}' alt='BevyChat Logo' class='img-fluid mx-auto' />
-                <h1 class='title'>Bev·y</h1>
-                <h2><span class="text-muted">/ˈbevē/</span></h2>
-                <blockquote>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 text-right">
+                <h1 class='title'>Bevy</h1>
+                <h2><span class="text-muted pr-2">/ˈbevē/</span></h2>
+                <blockquote class='mr-4'>
                     <p>
                         <h3><em>noun</em></h3>
                     </p>
                     <p>
-                        <ol>
+                        <ol class='pr-3'>
                             <li>
                                 <span class="text-muted">1.</span> large group or collection
                             </li>
                             <li>
                                 <span class="text-muted">2.</span> a group of animals and especially quail
                             </li>
+                        </ul>
+                    </p>
+                </blockquote>
+            </div>
+
+            <div class="col-6">
+                <h1 class='title'>Chat</h1>
+                <h2><span class="text-muted pl-2">/CHat/</span></h2>
+                <blockquote class='ml-4'>
+                    <p>
+                        <h3><em>verb</em></h3>
+                    </p>
+                    <p>
+                        <ol class='pl-3'>
                             <li>
-                                <span class="text-muted">3.</span> a pretty nice video chat service
+                                <span class="text-muted">1.</span> talk in a friendly and informal way.
+                            </li>
+                            <li>
+                                <span class="text-muted">2.</span> to take part in an online discussion in a chat room
                             </li>
                         </ul>
                     </p>
