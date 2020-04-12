f4ce9d1f444a349045e65c39711d42e2424003f1
Jacob Rogaishio <me@jacobrogaishio.com>
2020-04-12 16:55:50 -0400

    Added touch support for drag

--- public/js/app.js
+++ public/js/app.js
@@ -2330,7 +2330,7 @@ navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || nav
       },
       peerStreams: [],
       server: {
-        ip: 'bevy.chat',
+        ip: 'devbevy.chat',
         port: 1337,
         signal: null
       },
@@ -69140,26 +69140,46 @@ Vue.directive('draggable', {
     el.style.position = 'absolute';
     var startX, startY, initialMouseX, initialMouseY;
 
-    function mousemove(e) {
-      var dx = e.clientX - initialMouseX;
-      var dy = e.clientY - initialMouseY; //30x30 deadzone to
+    function onMove(x, y) {
+      var dx = x - initialMouseX;
+      var dy = y - initialMouseY; //30x30 deadzone to
 
       if (Math.abs(dx) < 30 && Math.abs(dy) < 30) {
+        console.log("Deadzoned?");
         return false;
-      }
+      } //console.log("TOP: " + (startY + dy) + "LEFT: " + (startX + dx));
+
 
       el.style.cssText = 'top: ' + (startY + dy) + 'px !important;left: ' + (startX + dx) + 'px !important; right: unset !important; bottom: unset !important';
       el.setAttribute("is-dragging", true);
       return false;
     }
 
-    function mouseup() {
-      document.removeEventListener('mousemove', mousemove);
-      document.removeEventListener('mouseup', mouseup); //Dirty way to keep the attribute incase there's any trickle down events
+    function mousemove(e) {
+      onMove(e.clientX, e.clientY);
+    }
 
+    function touchmove(e) {
+      onMove(e.touches[0].clientX, e.touches[0].clientY);
+    }
+
+    function movedone() {
+      //Dirty way to keep the attribute incase there's any trickle down events
       setTimeout(function () {
         el.removeAttribute("is-dragging");
       }, 100);
+    }
+
+    function mouseup() {
+      document.removeEventListener('mousemove', mousemove);
+      document.removeEventListener('mouseup', mouseup);
+      movedone();
+    }
+
+    function touchend() {
+      document.removeEventListener('touchmove', mousemove);
+      document.removeEventListener('touchend', mouseup);
+      movedone();
     }
 
     el.addEventListener('mousedown', function (e) {
@@ -69169,6 +69189,15 @@ Vue.directive('draggable', {
       initialMouseY = e.clientY;
       document.addEventListener('mousemove', mousemove);
       document.addEventListener('mouseup', mouseup);
+      return false;
+    });
+    el.addEventListener('touchstart', function (e) {
+      startX = el.offsetLeft;
+      startY = el.offsetTop;
+      initialMouseX = e.touches[0].clientX;
+      initialMouseY = e.touches[0].clientY;
+      document.addEventListener('touchmove', touchmove);
+      document.addEventListener('touchend', touchend);
       return false;
     });
   }
--- resources/js/directives/Draggable.js
+++ resources/js/directives/Draggable.js
@@ -1,41 +1,74 @@
 
 Vue.directive('draggable', {
     update: function (el) {
-      el.style.position = 'absolute';
-      var startX, startY, initialMouseX, initialMouseY;
+        el.style.position = 'absolute';
+        var startX, startY, initialMouseX, initialMouseY;
 
-      function mousemove(e) {
-        var dx = e.clientX - initialMouseX;
-        var dy = e.clientY - initialMouseY;
+        function onMove(x, y) {
+            var dx = x - initialMouseX;
+            var dy = y - initialMouseY;
 
+            //30x30 deadzone to
+            if(Math.abs(dx) < 30 && Math.abs(dy) < 30) {
+                console.log("Deadzoned?");
+                return false;
+            }
+            //console.log("TOP: " + (startY + dy) + "LEFT: " + (startX + dx));
 
-        //30x30 deadzone to
-        if(Math.abs(dx) < 30 && Math.abs(dy) < 30) {
+            el.style.cssText = 'top: ' + (startY + dy) + 'px !important;left: ' + (startX + dx) + 'px !important; right: unset !important; bottom: unset !important';
+            el.setAttribute("is-dragging", true);
             return false;
         }
-        el.style.cssText = 'top: ' + (startY + dy) + 'px !important;left: ' + (startX + dx) + 'px !important; right: unset !important; bottom: unset !important';
-        el.setAttribute("is-dragging", true);
-        return false;
-      }
 
-      function mouseup() {
-        document.removeEventListener('mousemove', mousemove);
-        document.removeEventListener('mouseup', mouseup);
-        //Dirty way to keep the attribute incase there's any trickle down events
-        setTimeout(function() {
-            el.removeAttribute("is-dragging");
-        }, 100);
+        function mousemove(e) {
+            onMove(e.clientX, e.clientY);
+        }
 
-      }
+        function touchmove(e) {
+            onMove(e.touches[0].clientX, e.touches[0].clientY);
+        }
 
-      el.addEventListener('mousedown', function(e) {
-        startX = el.offsetLeft;
-        startY = el.offsetTop;
-        initialMouseX = e.clientX;
-        initialMouseY = e.clientY;
-        document.addEventListener('mousemove', mousemove);
-        document.addEventListener('mouseup', mouseup);
-        return false;
-      });
+        function movedone() {
+            //Dirty way to keep the attribute incase there's any trickle down events
+            setTimeout(function() {
+                el.removeAttribute("is-dragging");
+            }, 100);
+          }
+
+          function mouseup() {
+            document.removeEventListener('mousemove', mousemove);
+            document.removeEventListener('mouseup', mouseup);
+
+            movedone();
+        }
+
+        function touchend() {
+            document.removeEventListener('touchmove', mousemove);
+            document.removeEventListener('touchend', mouseup);
+
+            movedone();
+        }
+
+        el.addEventListener('mousedown', function(e) {
+            startX = el.offsetLeft;
+            startY = el.offsetTop;
+            initialMouseX = e.clientX;
+            initialMouseY = e.clientY;
+            document.addEventListener('mousemove', mousemove);
+            document.addEventListener('mouseup', mouseup);
+
+            return false;
+        });
+
+        el.addEventListener('touchstart', function(e) {
+            startX = el.offsetLeft;
+            startY = el.offsetTop;
+            initialMouseX = e.touches[0].clientX;
+            initialMouseY = e.touches[0].clientY
+
+            document.addEventListener('touchmove', touchmove);
+            document.addEventListener('touchend', touchend);
+            return false;
+        });
     }
   });
