2a4770e8f0e2452b8df7ccc30892dd8ab2a94658
Jacob Rogaishio <me@jacobrogaishio.com>
2020-04-13 18:41:29 -0400

    Defer is important

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -17,7 +17,7 @@
 </script>
 
 <!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/app.js') }}" defer></script>
 
 <!-- Fonts -->
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
